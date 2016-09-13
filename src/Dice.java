6a24c1b1952b7f402179075c163c2a396abe1c1e
Daniel Schneider <daniel.p.schneider@gmail.com>
2016-09-13 17:07:30 -0400

    version2

--- src/Dice.java
+++ src/Dice.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 
 public class Dice {
-
+	       
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+         
+		int die1;
+		int die2;
+		int roll = 0;
+		
+		die1 = (int)(Math.random()*6) + 1;
+		die2 = (int)(Math.random()*6) + 1;
+		
+		System.out.println("The first die comes up " + die1);
+		System.out.println("The second die comes up " + die2);
+		System.out.println("Your total roll is " + roll);
+		
 	}
-
 }
+          
